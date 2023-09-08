e2f51e62aa570b5439624553244a442bf75fa220
William <willxis@gmail.com>
2023-09-08 18:22:00 -0300

    Spring Boot added to this project

--- core/src/main/java/academy/learnprogramming/GameImpl.java
+++ core/src/main/java/academy/learnprogramming/GameImpl.java
@@ -1,14 +1,13 @@
 package academy.learnprogramming;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 @Slf4j
 @Getter
--- core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
+++ core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
@@ -1,10 +1,9 @@
 package academy.learnprogramming;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
 
 @Slf4j
 @Component
